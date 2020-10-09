2ca9981569d55b86a88d228ff64ff3c6c285243b
Thierry Garcin <thierry.garcin@teamwork.net>
2020-10-09 12:30:35 +0200

    Fixed color escape character length issue

--- oh-my-aws-vault.plugin.zsh
+++ oh-my-aws-vault.plugin.zsh
@@ -1,6 +1,6 @@
 vault_info() {
     if (( ${+AWS_VAULT} ));
     then
-        echo "[%{$fg_bold[yellow]$AWS_VAULT%}$fg_no_bold[white]] ";
+        echo "[%{$fg_bold[yellow]%}$AWS_VAULT%{$fg_no_bold[white]%}] ";
     fi
 }
